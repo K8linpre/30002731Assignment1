c952fa24914c1cdffc6d7129c04ed52868f225bd
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-08-13 11:34:19 +1200

    program

---
+++ Question 4/Question 4/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_4
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int YearOfBirth;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string GetFullName()
+        {
+            return firstName + " " + lastName;
+        }
+        public int GetAge()
+        {
+            return DateTime.Now.Year - YearOfBirth;
+        }
+    }
+}
---
+++ Question 4/Question 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Question_4;
+
+namespace Question_4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Person one = new Person();
+
+            Console.WriteLine("Enter the name of the new person below");
+            Console.WriteLine("First name: ");
+            one.FirstName = (Console.ReadLine());
+            Console.WriteLine("Last name: ");
+            one.LastName = (Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Creating new person record ...");
+            Console.WriteLine("Enter year of birth: ");
+            one.YearOfBirth = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.WriteLine($"Person: {one.GetFullName()} is {one.GetAge()} years old.");
+
+        }
+    }
+}
