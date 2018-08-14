04861602fd28bd9f016775f2ef225330f16759ca
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-08-15 10:09:47 +1200

    program

---
+++ Question5/Question5/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question5
+{
+    class Person
+    {
+        Student;
+        public string firstName;
+        public string lastName;
+        public int YearOfBirth;
+
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string GetFullName()
+        {
+            return firstName + " " + lastName;
+        }
+        public int GetAge()
+        {
+            return DateTime.Now.Year - YearOfBirth;
+        }
+    }
+    public class Student
+    {
+        public static void StudentID()
+        {
+
+        }          
+    }
+}
+
+            
+
